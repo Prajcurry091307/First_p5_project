ee4fbf7a264bbaeed2883c9dcdba6a84f5c59c3c
Prajcurry091307 <77421995+Prajcurry091307@users.noreply.github.com>
2021-09-02 18:48:49 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+}
+
+function setup()
+{
+    createCanvas(300,300);
+    
+}
+
+function draw(){
+    fill(255,0,0);
+    //strokWeight(6);
+    stroke(224, 22, 22);
+    circle(30,30,20);
+}
